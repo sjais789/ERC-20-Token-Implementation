e4481848d1e3d09f57e949a5f8bf81c62309447e
Shubham <sjais789@users.noreply.github.com>
2019-01-13 07:18:07 +0100

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -15,3 +15,17 @@ uint public decimals = 4;
 
 
 uint public supply;
+
+uint public supply;
+
+address public owner;
+
+mapping(address => uint) public amount;
+
+mapping(address => mapping(address=>uint)) allowed;
+
+constructor() public {
+ supply =10000;
+ owner = msg.sender;
+ amount[owner] = supply;
+}
